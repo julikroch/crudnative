0e1e6b86f25b718a67b9c4b77abd9ba4326d2c98
Julian Krochmal <juliankrochmal@gmail.com>
2023-07-17 09:29:23 -0300

    feat(crud): setting react navigation

---
+++ views/ClientDetails.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import {Text} from 'react-native'
+
+const ClientDetails = () => {
+  return <Text>ClientDetails</Text>
+}
+
+export default ClientDetails
---
+++ views/NewClient.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import {Text} from 'react-native'
+
+const NewClient = () => {
+  return <Text>NewClient</Text>
+}
+
+export default NewClient
